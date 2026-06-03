540d42e93b5961f3a53b19ab3b8e73f7f10a69de
RamonCoost <ramoncoost@gmail.com>
2026-06-03 15:30:56 -0300

    chore: initial project setup

---
+++ src/test/java/io/github/ramon/ReadFlow/ReadFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ramon.ReadFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
